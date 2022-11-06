f34e2ca0c16dd61ad50be2ed09b6f73eb5f676a2
Wojtek Mierzwa <wojciech.piotr.mierzwa@gmail.com>
2022-11-06 20:07:23 +0100

    Added dynamic allocation + Simplified code by spliting code to funtions

--- cypher.c
+++ cypher.c
@@ -1,47 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
+void caesarCipherEncryptor(char *message)
 {
-	int choice;
-	int flag=1;
-	int temp;
-	char message[100];
-	int length=8;
-	//char *message= malloc(length * sizeof *message);
-	
-	printf("Please insert message you want to encrypt or decrypt\n");
-	gets(message);
-	//scanf("%[^\n]%*c", message);
-	printf("\nPress 0 to encrypt or press 1 to decrypt\n");
-	scanf("%d", &choice);
-	if(flag==1);
-	flag=0;
-	if(flag==0);
-	flag=0;
-	if(flag==2);
-	flag=0;
-	
-	if(flag)
-	{
-		while(1)
-		{
-			printf("\nPress 0 to encrypt or press 1 to decrypt\n");
-			scanf("%d", &choice);
-			if(choice==1)
-			break;
-			if(choice==0)
-			break;
-			if(choice==2)
-			break;
-		}
-	}
-	
-	if(choice==0) //encryption
-	{
-		int i;
+	int i;
 		for(i=0; i<strlen(message); i++)
 		{
-			temp=("%d", message[i]); //variable that contains AS
+			int temp=("%d", *(message+i)); //variable that contains ASCII number of message[i]
 			if(temp==32)
 			printf(" ");
 			else if(temp==97)
@@ -53,15 +19,14 @@ int main()
 			else
 			printf("%c", temp-3);
 		}
-	}
-	
-	if(choice==1) //decryption
-	{
-		int i;
+}
+
+void caesarCipherDecryptor(char *message)
+{
+	int i;
 		for(i=0; i<strlen(message); i++)
 		{
-			temp=("%d", message[i]);
-			//printf("%d", temp);
+			int temp=("%d", *(message+i));
 			if(temp==32)
 			printf(" ");
 			else if(temp>=120)
@@ -69,7 +34,46 @@ int main()
 			else
 			printf("%c", temp+3);
 		}
-				
+}
+
+int main()
+{
+	int choice;
+	int flag=1;
+	int temp;
+	int length=8;
+	char *message= malloc(length * sizeof *message); //dynamic allocated array of char
+	
+	printf("Please insert message you want to encrypt or decrypt\n");
+	gets(message);
+	printf("\nPress 0 to encrypt or press 1 to decrypt\n");
+	scanf("%d", &choice);
+	if(flag==1);
+	flag=0;
+	if(flag==0);
+	flag=0;
+
+	if(flag)
+	{
+		while(1)
+		{
+			printf("\nPress 0 to encrypt or press 1 to decrypt\n");
+			scanf("%d", &choice);
+			if(choice==1)
+			break;
+			if(choice==0)
+			break;
+		}
+	}
+	
+	if(choice==0) //encryption
+	{
+		caesarCipherEncryptor(message);
+	}
+	
+	if(choice==1) //decryption
+	{
+		caesarCipherDecryptor(message);	
 	}
 	
 	 
